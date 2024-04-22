02d7c341b081d2de7863847748ce8bda56d3608c
ethan-heo <ethan.hb94@gmail.com>
2024-04-22 16:27:31 +0900

    refactor: 토큰 파서 테스트코드를 리팩토링한다

--- scripts/generateTokenToCss/parser.test.mjs
+++ scripts/generateTokenToCss/parser.test.mjs
@@ -14,69 +14,16 @@ const TOKEN = {
 			},
 		},
 	},
-	값_형식이_문자열_형태: {
-		"color-primary": {
-			$type: "color",
-			$value: "#ffffff",
-		},
-		"color-text": {
-			$type: "color",
-			$value: "#000000",
-		},
-	},
-	값_형식이_참조_형태: {
-		"color-primary": {
-			$type: "color",
-			$value: "{color.white}",
-		},
-		"color-text": {
-			$type: "color",
-			$value: "{color.black}",
-		},
-	},
-	값_형식이_객체이고_문자열_형태: {
-		"primary-button": {
-			$type: "composition",
-			$value: {
-				"background-color": "#ffffff",
-				color: "#000000",
-			},
-		},
-	},
-	값_형식이_객체이고_참조_형태: {
-		"primary-button": {
-			$type: "composition",
-			$value: {
-				"background-color": "{color.white}",
-				color: "{color.black}",
-			},
-		},
-	},
-	값_형식이_객체이고_존재하지_않는_값: {
-		"primary-button": {
-			$type: "composition",
-			$value: {
-				"background-color": "{color.DO_NOT_HAVE}",
-				color: "{color.DO_NOT_HAVE}",
-			},
-		},
-	},
-	값_형식이_객체이고_값이_문자열이_아닌_형태: {
-		"primary-button": {
-			$type: "composition",
-			$value: {
-				"background-color": {},
-				color: 1,
-			},
-		},
-	},
 };
 
 describe(`토큰 $value 파싱`, () => {
 	const parser = parseToken(TOKEN.BASE);
 
 	it(`토큰의 $value가 문자열 형식이고 참조 형식일 때 baseToken에서 값을 찾아 변환한다.`, () => {
-		const value1 = TOKEN.값_형식이_참조_형태["color-primary"];
+		const value1 = {
+			$type: "color",
+			$value: "{color.white}",
+		};
 		const expected1 = {
 			$type: "color",
 			$value: "#ffffff",
@@ -84,7 +31,10 @@ describe(`토큰 $value 파싱`, () => {
 
 		expect(parser(value1)).toStrictEqual(expected1);
 
-		const value2 = TOKEN.값_형식이_참조_형태["color-text"];
+		const value2 = {
+			$type: "color",
+			$value: "{color.black}",
+		};
 		const expected2 = {
 			$type: "color",
 			$value: "#000000",
@@ -93,7 +43,10 @@ describe(`토큰 $value 파싱`, () => {
 	});
 
 	it(`토큰의 $value가 문자열 형식일 때 값을 그대로 반환한다.`, () => {
-		const value1 = TOKEN.값_형식이_문자열_형태["color-primary"];
+		const value1 = {
+			$type: "color",
+			$value: "#ffffff",
+		};
 		const expected1 = {
 			$type: "color",
 			$value: "#ffffff",
@@ -101,7 +54,10 @@ describe(`토큰 $value 파싱`, () => {
 
 		expect(parser(value1)).toStrictEqual(expected1);
 
-		const value2 = TOKEN.값_형식이_문자열_형태["color-text"];
+		const value2 = {
+			$type: "color",
+			$value: "#000000",
+		};
 		const expected2 = {
 			$type: "color",
 			$value: "#000000",
@@ -110,7 +66,13 @@ describe(`토큰 $value 파싱`, () => {
 	});
 
 	it(`토큰의 $value가 객체 형식일 때 값이 참조 형식이면 baseToken에서 값을 찾아 변환한다.`, () => {
-		const value = TOKEN.값_형식이_객체이고_참조_형태["primary-button"];
+		const value = {
+			$type: "composition",
+			$value: {
+				"background-color": "{color.white}",
+				color: "{color.black}",
+			},
+		};
 		const expected = {
 			$type: "composition",
 			$value: {
@@ -123,7 +85,13 @@ describe(`토큰 $value 파싱`, () => {
 	});
 
 	it(`토큰의 $value가 객체 형식일 때 값이 문자열 형식이면 값을 그대로 반환한다.`, () => {
-		const value = TOKEN.값_형식이_객체이고_문자열_형태["primary-button"];
+		const value = {
+			$type: "composition",
+			$value: {
+				"background-color": "#ffffff",
+				color: "#000000",
+			},
+		};
 		const expected = {
 			$type: "composition",
 			$value: {
@@ -136,15 +104,32 @@ describe(`토큰 $value 파싱`, () => {
 	});
 
 	it(`토큰의 $value가 객체 형식이고 값이 문자열이 아니면 에러를 내뱉는다.`, () => {
-		const value =
-			TOKEN.값_형식이_객체이고_값이_문자열이_아닌_형태["primary-button"];
+		const value = {
+			$type: "composition",
+			$value: {
+				"background-color": {},
+				color: 1,
+			},
+		};
 
 		expect(() => parser(value)).toThrowError("값이 문자열이 아닙니다.");
 	});
 
 	it(`토큰의 $value의 값이 문자열이 참조 형식이고 baseToken에서 값을 찾지 못하면 에러를 내뱉는다. `, () => {
-		const value = TOKEN.값_형식이_객체이고_존재하지_않는_값["primary-button"];
+		const value = {
+			$type: "composition",
+			$value: {
+				"background-color": "{color.DO_NOT_HAVE}",
+				color: "{color.DO_NOT_HAVE}",
+			},
+		};
 
 		expect(() => parser(value)).toThrowError("토큰값이 존재하지 않습니다.");
 	});
+});
+
+describe(`토큰 규칙`, () => {
+	const parser = parseToken(TOKEN.BASE);
+
+	it(`토큰 객체에 `, () => {});
 });
