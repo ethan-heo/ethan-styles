e63bf7ee43cbe7c474a48c747e182b9904260adb
ethan-heo <ethan.hb94@gmail.com>
2024-04-26 13:53:02 +0900

    feat: mapper를 구현한다

--- scripts/generateToken/index.ts
+++ scripts/generateToken/index.ts
@@ -1,2 +1,13 @@
 import globalToken from "../../design-token/global.tokens.json";
 import lightThemeToken from "../../design-token/light.themes.tokens.json";
+import mapper from "./mapper";
+import transformer from "./transformer";
+
+const transformedToken = transformer(lightThemeToken, [
+	lightThemeToken,
+	globalToken,
+]);
+
+const mappedToken = mapper(transformedToken);
+
+console.log(JSON.stringify(mappedToken, null, 2));
