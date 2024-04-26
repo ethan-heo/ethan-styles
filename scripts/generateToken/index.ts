bfb98b39cc38bb8eb58a18f8fabaa71786471123
ethan-heo <ethan.hb94@gmail.com>
2024-04-26 15:06:17 +0900

    feat: 토큰을 css 파일로 생성하도록 구현한다

--- scripts/generateToken/index.ts
+++ scripts/generateToken/index.ts
@@ -1,16 +1,36 @@
 import globalToken from "../../design-token/global.tokens.json";
 import lightThemeToken from "../../design-token/light.themes.tokens.json";
+import creator from "./creator";
+import { Token } from "./generateToken.token";
 import mapper from "./mapper";
 import parser from "./parser";
 import transformer from "./transformer";
 
-const transformedToken = transformer(lightThemeToken, [
-	lightThemeToken,
-	globalToken,
-]);
+type GenerateTokenOptions = {
+	token: Token;
+	baseTokens: Token[];
+	fileName: string;
+};
 
-const mappedToken = mapper(transformedToken);
+const generateToken = async ({
+	token,
+	baseTokens,
+	fileName,
+}: GenerateTokenOptions) => {
+	const transformedToken = transformer(token, baseTokens);
+	const mappedToken = mapper(transformedToken);
+	const parsedToken = parser(mappedToken, baseTokens);
 
-const parsedToken = parser(mappedToken, [lightThemeToken, globalToken]);
+	await creator(parsedToken, fileName);
+};
 
-console.log(JSON.stringify(parsedToken, null, 2));
+generateToken({
+	token: globalToken,
+	baseTokens: [globalToken],
+	fileName: "global",
+});
+generateToken({
+	token: lightThemeToken,
+	baseTokens: [lightThemeToken, globalToken],
+	fileName: "light-theme",
+});
