304d7147915448348fda9f63255a9832bfd595f8
ethan-heo <ethan.hb94@gmail.com>
2024-09-10 16:44:19 +0900

    chore: build

--- packages/ethan-ui/types/react/Form/useFormState/index.d.ts
+++ packages/ethan-ui/types/react/Form/useFormState/index.d.ts
@@ -1,20 +1,3 @@
 import { Params, State } from "./useFormState.types";
-declare const useFormState: <P extends Params<any>, S extends State<P["form"]>>(prop: P, initializedState?: S) => {
-    form: {
-        [x: string]: {
-            element: {
-                name: string;
-                id: string;
-                value: any;
-                onChange: (e: React.ChangeEvent<HTMLElement>) => void;
-                onBlur: (e: React.ChangeEvent<HTMLElement>) => void;
-            };
-            validationEvent: import("./useFormState.types").ValidationEvent;
-            error?: undefined;
-            reset: (value?: any) => void;
-            change: (value: any) => void;
-        };
-    };
-    onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-};
+declare const useFormState: <P extends Params<any>, S extends State<P["form"]>>(prop: P, initializedState?: S) => S;
 export default useFormState;
