c01296e81f26dc4dc0b7939f1d8697a45077de78
ethan-heo <ethan.hb94@gmail.com>
2024-09-24 21:09:49 +0900

    refactor: 디자인 토큰 생성 로직의 관심사 분리를 위해 리팩토링한다

--- packages/ethan-ui/scripts/generateToken/creator.ts
+++ packages/ethan-ui/scripts/generateToken/creator.ts
@@ -1,41 +1,34 @@
-import path from "path";
 import ejs from "ejs";
-import { TokenObj } from "./generateToken.token";
+import { Token, TokenObj } from "./generateToken.token";
 import prettier from "prettier";
 import fs from "fs/promises";
 
-const TEMPLATE_PATH = path.resolve(__dirname, "../../design-token/templates");
-const TEMPLATE_FILES = {
-	variables: path.resolve(TEMPLATE_PATH, "variables.ejs"),
-	constants: path.resolve(TEMPLATE_PATH, "constants.ejs"),
-};
-const OUTPUT_PATHS = {
-	variables: path.resolve(__dirname, "../../src/tokens"),
-	constants: path.resolve(__dirname, "../../src/constants"),
-};
-
-const creator = async (
-	generatedTokens: Record<string, TokenObj>,
-	fileName: string,
-	type: keyof typeof TEMPLATE_FILES,
-) => {
+const creator = async ({
+	token,
+	fileName,
+	outputPath,
+	templatePath,
+}: {
+	token: Record<string, Token | TokenObj>;
+	fileName: string;
+	outputPath: string;
+	templatePath: string;
+}) => {
 	const contents = await ejs.renderFile(
-		TEMPLATE_FILES[type],
-		{ variables: Object.entries(generatedTokens) },
+		templatePath,
+		{ variables: Object.entries(token) },
 		{ async: true },
 	);
-	const output = OUTPUT_PATHS[type];
-
-	await fs.mkdir(output, { recursive: true });
-
-	if (type === "variables") {
-		const formattedContents = await prettier.format(contents, {
-			parser: "css",
-		});
-		await fs.writeFile(`${output}/${fileName}`, formattedContents, "utf-8");
-	} else {
-		await fs.writeFile(`${output}/${fileName}`, contents, "utf-8");
-	}
+	const ext = fileName.slice(fileName.lastIndexOf(".") + 1);
+	const EXT_MAP = {
+		ts: "typescript",
+		css: "css",
+	};
+	const formattedContents = await prettier.format(contents, {
+		parser: EXT_MAP[ext],
+	});
+	await fs.mkdir(outputPath, { recursive: true });
+	await fs.writeFile(`${outputPath}/${fileName}`, formattedContents, "utf-8");
 };
 
 export default creator;
--- packages/ethan-ui/scripts/generateToken/index.ts
+++ packages/ethan-ui/scripts/generateToken/index.ts
@@ -1,29 +1,67 @@
+import path from "path";
 import globalToken from "../../design-token/global.tokens.json";
 import lightThemeToken from "../../design-token/light.themes.tokens.json";
 import creator from "./creator";
 import mapper from "./mapper";
 import generateDesignToken from "generate-design-token";
 
+const TEMPLATE_PATH = path.resolve(__dirname, "../../design-token/templates");
+const TEMPLATE_FILES = {
+	VARIABLES: path.resolve(TEMPLATE_PATH, "variables.ejs"),
+	CONSTANTS: path.resolve(TEMPLATE_PATH, "constants.ejs"),
+	TYPES: path.resolve(TEMPLATE_PATH, "types.ejs"),
+};
+const OUTPUT_PATHS = {
+	VARIABLES: path.resolve(__dirname, "../../src/tokens"),
+	CONSTANTS: path.resolve(__dirname, "../../src/constants"),
+	TYPES: path.resolve(__dirname, "../../src/types"),
+};
+
 const generate = async () => {
-	await creator(
-		mapper(generateDesignToken(globalToken, [globalToken])),
-		"global.css",
-		"variables",
-	);
-	await creator(
-		mapper(
+	const GENERATED_DESIGN_TOKEN_MAP = {
+		GLOBAL: mapper(generateDesignToken(globalToken, [globalToken])),
+		LIGHT_THEME: mapper(
 			generateDesignToken(lightThemeToken, [lightThemeToken, globalToken]),
 		),
-		"light-theme.css",
-		"variables",
-	);
-	await creator(
-		mapper(
-			generateDesignToken(lightThemeToken, [lightThemeToken, globalToken]),
-		),
-		"light-theme.constants.ts",
-		"constants",
-	);
+	};
+
+	await creator({
+		token: GENERATED_DESIGN_TOKEN_MAP.GLOBAL,
+		fileName: "global.css",
+		outputPath: OUTPUT_PATHS.VARIABLES,
+		templatePath: TEMPLATE_FILES.VARIABLES,
+	});
+	await creator({
+		token: GENERATED_DESIGN_TOKEN_MAP.LIGHT_THEME,
+		fileName: "light-theme.css",
+		outputPath: OUTPUT_PATHS.VARIABLES,
+		templatePath: TEMPLATE_FILES.VARIABLES,
+	});
+	await creator({
+		token: GENERATED_DESIGN_TOKEN_MAP.LIGHT_THEME,
+		fileName: "css-variables.d.ts",
+		outputPath: OUTPUT_PATHS.TYPES,
+		templatePath: TEMPLATE_FILES.TYPES,
+	});
+	// await creator(
+	// 	mapper(generateDesignToken(globalToken, [globalToken])),
+	// 	"global.css",
+	// 	"variables",
+	// );
+	// await creator(
+	// 	mapper(
+	// 		generateDesignToken(lightThemeToken, [lightThemeToken, globalToken]),
+	// 	),
+	// 	"light-theme.css",
+	// 	"variables",
+	// );
+	// await creator(
+	// 	mapper(
+	// 		generateDesignToken(lightThemeToken, [lightThemeToken, globalToken]),
+	// 	),
+	// 	"light-theme.constants.ts",
+	// 	"constants",
+	// );
 };
 
 generate();
