347525c2ec413b69d66e1cd6fc45e0a3c1cddfc7
ethan-heo <ethan.hb94@gmail.com>
2024-09-27 22:08:37 +0900

    chore: 가상돔 테스트 환경을 설정한다

--- packages/ethan-ui/configs/vitest.config.mjs
+++ packages/ethan-ui/configs/vitest.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
 	test: {
 		cache: false,
+		environment: "jsdom",
+		setupFiles: path.resolve(__dirname, "./vitest.setup.mjs"),
 	},
 });
