97082b40b238f965d590aa115c137ddadb38167d
ethan-heo <ethan.hb94@gmail.com>
2024-09-27 16:14:58 +0900

    refactor: 토큰 생성 로직을 개선한다

--- packages/ethan-ui/scripts/generateToken/index.ts
+++ packages/ethan-ui/scripts/generateToken/index.ts
@@ -6,55 +6,64 @@ import mapper from "./mapper";
 import generateDesignToken from "generate-design-token";
 import ejs from "ejs";
 import prettier from "prettier";
+import { Token } from "./generateToken.token";
 
-async function generateToken() {
-	const TOKEN_DIR_PATH = path.resolve(__dirname, "../../design-token/tokens");
-	const THEME_OUTPUT_PATH = path.resolve(
-		__dirname,
-		"../../src/styles/theme.css",
-	);
-	const CSS_THEME_TEMPLATE = path.resolve(
+const PATH = {
+	TOKEN_DIR: path.resolve(__dirname, "../../design-token/tokens"),
+	CSS_THEME_TEMPLATE: path.resolve(
 		__dirname,
 		"../../design-token/templates/css-theme.ejs",
-	);
-	const { found: tokenFilePaths } = new GlobSync(
-		TOKEN_DIR_PATH + `/**/*.token.json`,
-	);
-	const tokens = await Promise.all(
-		tokenFilePaths.map((path) => readFile(path, { encoding: "utf-8" })),
-	);
+	),
+	GLOBAL_THEME_VARIABLES_TEMPLATE: path.resolve(
+		__dirname,
+		"../../design-token/templates/global-theme-postcss-var.ejs",
+	),
+	CREATE_THEME_TEMPLATE: path.resolve(
+		__dirname,
+		"../../design-token/templates/create-theme.ejs",
+	),
+	THEME_INTERFACE_TEMPLATE: path.resolve(
+		__dirname,
+		"../../design-token/templates/theme-interface.ejs",
+	),
+	THEME_OUTPUT: path.resolve(__dirname, "../../src/styles/theme.css"),
+	GLOBAL_THEME_VARIABLES_OUTPUT: path.resolve(
+		__dirname,
+		"../../src/styles/global-theme-var.css",
+	),
+	THEME_INTERFACE_OUTPUT: path.resolve(
+		__dirname,
+		"../../src/types/css-variables.ts",
+	),
+};
 
-	const themes: string[] = [];
+interface Theme {
+	name: string;
+	token: Token;
+}
 
-	for (const stringifiedToken of tokens) {
-		const { name, token } = JSON.parse(stringifiedToken);
-		const content = await ejs.renderFile(CSS_THEME_TEMPLATE, {
-			name,
+async function generateToken(themes: Theme[]) {
+	const themeContents: string[] = [];
+
+	for (const theme of themes) {
+		const content = await ejs.renderFile(PATH.CSS_THEME_TEMPLATE, {
+			name: theme.name,
 			variables: Object.entries(
-				mapper(generateDesignToken(token, [globalToken, token])),
+				mapper(generateDesignToken(theme.token, [globalToken, theme.token])),
 			),
 		});
 
-		themes.push(content);
+		themeContents.push(content);
 	}
-
-	const formattedTheme = await prettier.format(themes.join("\n"), {
+	const formattedTheme = await prettier.format(themeContents.join("\n"), {
 		parser: "css",
 	});
 
-	await writeFile(THEME_OUTPUT_PATH, formattedTheme, "utf-8");
+	await writeFile(PATH.THEME_OUTPUT, formattedTheme, "utf-8");
 }
 
 async function generateGlobalThemeVariables() {
-	const GLOBAL_THEME_VARIABLES_TEMPLATE_PATH = path.resolve(
-		__dirname,
-		"../../design-token/templates/global-theme-postcss-var.ejs",
-	);
-	const GLOBAL_THEME_VARIABLES_OUTPUT_PATH = path.resolve(
-		__dirname,
-		"../../src/styles/global-theme-var.css",
-	);
-	const contents = await ejs.renderFile(GLOBAL_THEME_VARIABLES_TEMPLATE_PATH, {
+	const contents = await ejs.renderFile(PATH.GLOBAL_THEME_VARIABLES_TEMPLATE, {
 		variables: Object.entries(
 			mapper(generateDesignToken(globalToken, [globalToken])),
 		),
@@ -62,44 +71,46 @@ async function generateGlobalThemeVariables() {
 	const formattedTheme = await prettier.format(contents, {
 		parser: "css",
 	});
-	await writeFile(GLOBAL_THEME_VARIABLES_OUTPUT_PATH, formattedTheme, "utf-8");
+	await writeFile(PATH.GLOBAL_THEME_VARIABLES_OUTPUT, formattedTheme, "utf-8");
 }
 
-async function generateThemeVariablesInterface() {
-	const CREATE_THEME_TEMPLATE_PATH = path.resolve(
-		__dirname,
-		"../../design-token/templates/create-theme.ejs",
-	);
-	const THEME_INTERFACE_TEMPLATE_PATH = path.resolve(
-		__dirname,
-		"../../design-token/templates/theme-interface.ejs",
-	);
-	const THEME_INTERFACE_OUTPUT_PATH = path.resolve(
-		__dirname,
-		"../../src/types/css-variables.ts",
-	);
-
-	const emptyTheme = JSON.parse(
-		await ejs.renderFile(CREATE_THEME_TEMPLATE_PATH),
-	);
-	const emptyContent = await ejs.renderFile(THEME_INTERFACE_TEMPLATE_PATH, {
+async function generateThemeVariablesInterface(anyThemeToken: Token) {
+	const emptyContent = await ejs.renderFile(PATH.THEME_INTERFACE_TEMPLATE, {
 		variables: Object.entries(
-			mapper(
-				generateDesignToken(emptyTheme.token, [globalToken, emptyTheme.token]),
-			),
+			mapper(generateDesignToken(anyThemeToken, [globalToken, anyThemeToken])),
 		),
 	});
 
 	const formattedTheme = await prettier.format(emptyContent, {
 		parser: "typescript",
 	});
-	await writeFile(THEME_INTERFACE_OUTPUT_PATH, formattedTheme, "utf-8");
+	await writeFile(PATH.THEME_INTERFACE_OUTPUT, formattedTheme, "utf-8");
 }
 
 async function generate() {
-	await generateToken();
+	const themes = await getThemes();
+
+	if (themes.length === 0) throw new Error(`테마를 찾을 수 없습니다.`);
+
+	await generateToken(themes);
+	await generateThemeVariablesInterface(themes[0].token);
 	await generateGlobalThemeVariables();
-	await generateThemeVariablesInterface();
 }
 
 generate();
+
+async function getThemes() {
+	const { found: tokenFilePaths } = new GlobSync(
+		PATH.TOKEN_DIR + `/**/*.token.json`,
+	);
+	const tokens = await Promise.all(
+		tokenFilePaths.map((path) => readFile(path, { encoding: "utf-8" })),
+	);
+	const themes: Theme[] = [];
+
+	for (const stringifiedToken of tokens) {
+		themes.push(JSON.parse(stringifiedToken));
+	}
+
+	return themes;
+}
